8b8015c00ca1ba82163dec8b960d2b301ed977bb
Rahul Sahay <rahulsahay19@hotmail.com>
2017-12-17 18:20:42 +0530

    Template driven finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm:NgForm;
+  reply='';
+  gender = ['Male','Female',"Don't want to disclose" ];
   checkUserName() {
     const availableName = 'admin';
   }
 
-  onSubmit(form:NgForm){
-    console.log(form);
+  // onSubmit(form:NgForm){
+  //   console.log(form);
+  // }
+
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
